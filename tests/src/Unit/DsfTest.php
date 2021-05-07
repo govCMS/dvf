67faa2d635586e9bfd4fb5cb2047de1a7edfff03
Wilson Wu <wwu@doghouse.agency>
2021-05-07 13:40:34 +1000

    Create a sample unit test for DSF

---
+++ tests/src/Unit/DsfTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\Tests\dvf\Unit;
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+
+/**
+ * Tests dsf.
+ *
+ * @group doghouse
+ */
+class DsfTest extends UnitTestCase
+{
+  /**
+   * Test data for rendering.
+   *
+   * @var array
+   */
+  protected $testData = [
+      //users info
+      [
+        'Username' => 'name1',
+        'Id' => 'id1',
+        'First name' => 'firstname1',
+        'Last name' => 'last1',
+      ],
+      [
+        'Username' => 'name2',
+        'Id' => 'id2',
+        'First name' => 'firstname2',
+        'Last name' => 'last2',
+      ],
+      [
+        'Username' => 'name3',
+        'Id' => 'id3',
+        'First name' => 'firstname3',
+        'Last name' => 'last3',
+      ]
+  ];
+
+  protected function setUp() {
+    parent::setUp();
+
+    $container = new ContainerBuilder();
+    \Drupal::setContainer($container);
+    $container->set('string_translation', self::getStringTranslationStub());
+  }
+
+  /**
+   * Test input data.
+   */
+  public function testInputData()
+  {
+    $output = $this->testData;
+    $this->assertIsArray($output);
+  }
+}
