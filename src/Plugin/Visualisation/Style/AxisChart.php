0083cd78352a4785f19bc712cb793bd94ae33007
Wilson Wu <wwu@doghouse.agency>
2021-06-28 12:46:20 +1000

    add some comments

--- src/Plugin/Visualisation/Style/AxisChart.php
+++ src/Plugin/Visualisation/Style/AxisChart.php
@@ -853,7 +853,7 @@ abstract class AxisChart extends TableVisualisationStyleBase {
       }
     }
 
-    // Get row headers
+    // Get the row headers
     $ticks_custom = $settings['axis']['x']['tick']['values']['custom'] ?? [];
 
     // Data columns.
@@ -890,6 +890,7 @@ abstract class AxisChart extends TableVisualisationStyleBase {
     $tick_values_field = $this->config('axis', 'x', 'tick', 'values', 'field');
 
     if ($settings['axis']['x']['type'] === '' && !empty($record) && property_exists($record, $tick_values_field)) {
+      //Check whether $tick_values_field is a number and the $tick_values_field cannot be one of the row header
       is_numeric($record->{$tick_values_field}) && !in_array($record->{$tick_values_field}, $ticks_custom) ?
         $settings['axis']['x']['type'] = 'indexed' :
         $settings['axis']['x']['type'] = 'category';
